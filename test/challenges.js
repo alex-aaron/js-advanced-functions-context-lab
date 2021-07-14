715f8edd5f22c9d2960a95cb7f602db73ce31034
Alex <alexfrancisaaron@gmail.com>
2021-07-14 13:07:28 -0500

    finalized customSortStr function

--- test/challenges.js
+++ test/challenges.js
@@ -2,7 +2,20 @@ let sampleOrder = "cba";
 let sampleStr = "abcd";
 
 function customSortStr(order, str) {
-    
+    let returnStr;
+    let orderArr = order.split("");
+    let strArr = str.split("");
+    let remArr = [];
+    let count = 0;
+    for (let i = 0; i < strArr.length; i++) {
+        if (orderArr.includes(strArr[i]) && count < strArr.length) {
+            count += 1;
+        } else {
+            remArr.push(strArr[i]);
+        }
+    }
+    returnStr = orderArr.join("") + remArr.join("");
+    return returnStr;
 }
 
-customSortStr(sampleOrder, sampleStr);
+console.log(customSortStr(sampleOrder, sampleStr));
