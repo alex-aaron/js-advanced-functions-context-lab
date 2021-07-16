3c176d6b565d60be989c9ea3c04f6ab0abe23eef
Alex <alexfrancisaaron@gmail.com>
2021-07-16 16:20:18 -0500

    created for loop and inner while loop

--- test/challenges.js
+++ test/challenges.js
@@ -3,7 +3,15 @@
 let sampleStr = "abcabcbb";
 
 function lengthOfLongestSubstring(s) {
-  
+  let substringLength = 0;
+  let substringArr = [];
+  for (let i = 0; i < s.length; i++) {
+    let substring = s[i];
+    let count = 1;
+    while (substringArr.length < i + 1) {
+      
+    }
+  }
 }
 
 console.log(lengthOfLongestSubstring(sampleStr));
