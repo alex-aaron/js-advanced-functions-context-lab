c1fc76094fb42c371499ac4e8ffca347aaa3b90e
Alex <alexfrancisaaron@gmail.com>
2021-07-16 15:19:24 -0500

    created lengthOfLongestSubstring function

--- test/challenges.js
+++ test/challenges.js
@@ -1,22 +1,9 @@
 
 
-let sampleArr = [0, 2, 11, 7];
+let sampleStr = "abcabcbb";
 
-function arrSum(nums, target) {
-    let sumArr = [];
-  for (let i = 0; i < nums.length; i++) {
-    let sum = 0;
-    let count = 1;
-    while (count < nums.length) {
-      sum = nums[i] + nums[i + count];
-      if (sum === target) {
-        sumArr.push(i);
-        sumArr.push(i + count);
-      }
-      count ++;
-    }
-  }
-  return sumArr;
+function lengthOfLongestSubstring(str) {
+  return str;
 }
 
-console.log(arrSum(sampleArr));
+console.log(lengthOfLongestSubstring(sampleStr));
